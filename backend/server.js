02653d5c2854152c503619ae30b5e6488f5f2383
Marat <evolution99@rambler.ru>
2023-05-17 08:05:15 +0300

    Added code

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (request, response) => response.send("Server is ready"));
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is ready"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
